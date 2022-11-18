6821c01041310db071eeaa35f1ac22393d2a3aeb
IDmitryA <ivan4ik1986@gmail.com>
2022-11-18 19:27:21 +0200

    26  questions

--- bot/questions.py
+++ bot/questions.py
@@ -19,42 +19,53 @@ class Test:
             "B. I'm OK, thanks.\nC. I'm Mike, your new teammate.\n"
             "D. I'm American.": "C"
         },
-        {"7. ___ 20 people in this room.\n\nA. This is\n" "B. There is"
-         "\nC. They are\nD. There are": "D"},
-        {"8. Sorry, I can't speak. I ___ right now.\n\nA. driving\n" "B. drive\n"
-         "C. 'm driving\nD. drives": "C"},
-        {"9. I speak Spanish, but Lisa ___ .\n\nA. don't speaks\nB. doesn't\nC. not speaks\n"
-         "D. not speak": "B"},
+        {"7. ___ 20 people in this room.\n\nA. This is\n" "B. There is" "\nC. They are\nD. There are": "D"},
+        {"8. Sorry, I can't speak. I ___ right now.\n\nA. driving\n" "B. drive\n" "C. 'm driving\nD. drives": "C"},
+        {"9. I speak Spanish, but Lisa ___ .\n\nA. don't speaks\nB. doesn't\nC. not speaks\n" "D. not speak": "B"},
         {"10. She ___ at work last week.\n\nA. didn't be\nB. wasn't\nC. weren't\nD. isn't": "B"},
-        {"11. - ___ do you go to the gym? - Twice a week. .\n\nA. How often\nB. Why\nC. How\n"
-         "D. How many": "A"},
+        {"11. - ___ do you go to the gym? - Twice a week. .\n\nA. How often\nB. Why\nC. How\n" "D. How many": "A"},
         {"12. I like ___ before bedtime.\n\nA. read\nB. to reading\nC. am reading\nD. reading": "D"},
-        {"13. I ___ when I was 4 years old.\n\nA. can swim\nB. could swim\nC. could to swim\n"
-         "D. can't swim": "B"},
-        {"14. They ___ in the office when suddenly there was a blackout.\n\nA. worked\n"
-         "B. was working\nC. are working\nD. were working": "D"},
-        {"15. ___ this guy before? .\n\nA. Did you ever seen\nB. Are you ever see\n"
-         "C. Have you ever seen\nD. Do you ever see": "C"},
-        {"16. I went to the bookshop ___ 'Harry Potter'.\n\nA. for buy\nB. to buy\nC. to buying\n"
-         "D. for to buy": "B"},
-        {"17. We will stay at home if it ___ tomorrow. .\n\nA. rains\nB. will rain\n"
-         "C. raining\nD. is rain": "A"},
+        {"13. I ___ when I was 4 years old.\n\nA. can swim\nB. could swim\nC. could to swim\n" "D. can't swim": "B"},
+        {
+            "14. They ___ in the office when suddenly there was a blackout.\n\nA. worked\n"
+            "B. was working\nC. are working\nD. were working": "D"
+        },
+        {
+            "15. ___ this guy before? .\n\nA. Did you ever seen\nB. Are you ever see\n"
+            "C. Have you ever seen\nD. Do you ever see": "C"
+        },
+        {
+            "16. I went to the bookshop ___ 'Harry Potter'.\n\nA. for buy\nB. to buy\nC. to buying\n"
+            "D. for to buy": "B"
+        },
+        {"17. We will stay at home if it ___ tomorrow. .\n\nA. rains\nB. will rain\n" "C. raining\nD. is rain": "A"},
         {"18. Mike plays football ___ than me.\n\nA. more good\nB. more better\nC. best\nD. better": "D"},
-        {"19. He doesn't smoke now  but he ___ much 20 years ago.\n\nA. used to smoke\nB. was smoking\n"
-         "C. has smoked\nD. was smoked": "A"},
+        {
+            "19. He doesn't smoke now  but he ___ much 20 years ago.\n\nA. used to smoke\nB. was smoking\n"
+            "C. has smoked\nD. was smoked": "A"
+        },
         {"20. He passed his English evaluation very ___.\n\nA. easy\nB. easier\nC. good\nD. easily": "D"},
-        {"21. I can't stand ___ in a stuffy office.\n\nA.  to work\nB. working\nC. work\n"
-         "D. to working": "B"},
-        {"22. Let's go somewhere else. There's ___ noise in this room.\n\nA. too many\nB. too much\n"
-         "C. enough\nD. too": "B"},
-        {"23. It's going to be  a very long day. I won't get home ___ 9 pm.\n\nA. since\nB. to\n"
-         "C. towards\nD. until": "D"},
-        {"24. They usually ___ at home but today they ___ lunch in a restaurant.\n\nA. are eating, have\n"
-         "B. eat, have\nC. eat, are having\nD. are eating, are having": "C"},
-        {"25.  I think most people ___ English for their jobs in the future.\n\nA. need\nB. are needing\n"
-         "C. will need\nD. will needed": "C"},
-        {"26. I have received 20 messages from him ______.\n\nA. yesterday\nB. on Friday\nC. this week\n"
-         "D. two days ago": "C"}
+        {"21. I can't stand ___ in a stuffy office.\n\nA.  to work\nB. working\nC. work\n" "D. to working": "B"},
+        {
+            "22. Let's go somewhere else. There's ___ noise in this room.\n\nA. too many\nB. too much\n"
+            "C. enough\nD. too": "B"
+        },
+        {
+            "23. It's going to be  a very long day. I won't get home ___ 9 pm.\n\nA. since\nB. to\n"
+            "C. towards\nD. until": "D"
+        },
+        {
+            "24. They usually ___ at home but today they ___ lunch in a restaurant.\n\nA. are eating, have\n"
+            "B. eat, have\nC. eat, are having\nD. are eating, are having": "C"
+        },
+        {
+            "25.  I think most people ___ English for their jobs in the future.\n\nA. need\nB. are needing\n"
+            "C. will need\nD. will needed": "C"
+        },
+        {
+            "26. I have received 20 messages from him ______.\n\nA. yesterday\nB. on Friday\nC. this week\n"
+            "D. two days ago": "C"
+        },
     ]
     question_counter = 0
     count_right_answers = 0
